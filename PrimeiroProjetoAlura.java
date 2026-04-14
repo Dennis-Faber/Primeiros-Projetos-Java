7190e175fcec2f7908a0d6a38b98600609434a67
Dennis-Faber <Dennisfaberg@icloud.com>
2026-04-13 22:48:53 -0300

    Update and rename Desafio1Alura/Desafio1Alura.java to PrimeiroProjetoAlura.java

--- PrimeiroProjetoAlura.java
+++ PrimeiroProjetoAlura.java
@@ -1,6 +1,7 @@
+package PastaPrimeiroProjetoAlura;
 import java.util.Scanner;
 
-public class Desafio1Alura {
+public class PrimeiroProjetoAlura {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String tipoDeConta = "abc";
